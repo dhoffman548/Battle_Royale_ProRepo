9bc551b14d3b096eeebdba73fb66beb9464e8c3f
dhoffman548 <dylanhoffman651@gmail.com>
2021-10-01 02:41:12 -0700

    PLease
    I sleep

---
+++ Battle_Royale_Pro/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers = 10;
+
+    // instance 
+    public static NetworkManager instance;
+
+    void Awake ()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        // connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void CreateRoom (string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+
+    public void JoinRoom (string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    [PunRPC]
+    public void ChangeScene (string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        PhotonNetwork.LoadLevel("Menu");
+    }
+
+    public override void OnPlayerLeftRoom (Player otherPlayer)
+    {
+        GameManager.instance.alivePlayers--;
+        GameUI.instance.UpdatePlayerInfoText();
+
+        if(PhotonNetwork.IsMasterClient)
+        {
+            GameManager.instance.CheckWinCondition();
+        }
+
+    }
+}
